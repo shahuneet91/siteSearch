20c6931ac32d44dec3e5184c91d85e62f63ae230
Uneet <shahuneet91@gmail.com>
2017-02-27 11:04:36 -0600

    v1.0

---
+++ reservationApp/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace reservationApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult setValues(string location, string chkIn, string chkOut, string roomQty)
+        {
+            string loc = location;
+            return Content(location);
+        }
+    }
+}
---
+++ reservationApp/Models/reservationModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace reservationModel.Models
+{
+    public class reservationModel
+    {
+        public string loc { get; set; }
+        public string chkInDate { get; set; }
+        public string chkOutDate { get; set; }
+        public string room_Qty { get; set; }
+
+    }
+}
